d1844ec78c3c2f3b2b6e0cd2a7d86a01da2ed839
WildOnTheInside <58295644+WildOnTheInside@users.noreply.github.com>
2021-06-14 12:26:04 +0300

    Update auxiliary.hpp

--- source/auxiliary.hpp
+++ source/auxiliary.hpp
@@ -35,29 +35,14 @@ inline std::ostream& operator<<(std::ostream& out, const glm::dmat4& m) {
     out << m[0][1] << " " << m[1][1] << " " << m[2][1] << " " << m[3][1] << "\n";
     out << m[0][2] << " " << m[1][2] << " " << m[2][2] << " " << m[3][2] << "\n";
     out << m[0][3] << " " << m[1][3] << " " << m[2][3] << " " << m[3][3];
+    return out;
 }
 
 inline std::ostream& operator<<(std::ostream& out, const glm::dmat3& m) {
     out << m[0][0] << " " << m[1][0] << " " << m[2][0] << "\n";
     out << m[0][1] << " " << m[1][1] << " " << m[2][1] << "\n";
     out << m[0][2] << " " << m[1][2] << " " << m[2][2];
+    return out;
 }
-
-// bool equal(const glm::dvec3& a, const glm::dvec3& b) {
-//     return a == b;
-// }
-
-// bool cmp(const glm::dvec3& a, const glm::dvec3& b) {
-//     if (a.x != b.x) {
-//         return a.x < b.x;
-//     }
-//     if (a.y != b.y) {
-//         return a.y < b.y;
-//     }
-//     if (a.z != b.z) {
-//         return a.z < b.z;
-//     }
-//     return false;
-// }
 
 
